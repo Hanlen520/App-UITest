a4eb13750688700c8f8f8e56d2e589c1fb489e8d
圈圈 <quanquandequan@126.com>
2018-02-14 22:27:17 +0800

    删除没有用的code

--- ios_run.py
+++ ios_run.py
@@ -1,27 +1,16 @@
-#coding=utf-8
-import sys
+# coding=utf-8
 import time
 import unittest
-from importlib import reload
-
 from HTMLTestRunner import HTMLTestRunner
-
-#解决htmltestrunner中显示乱码的问题
-default_encoding = 'utf-8'
-if sys.getdefaultencoding() != default_encoding:
-    reload(sys)
-    sys.setdefaultencoding(default_encoding)
 
 #注意使用套件时，在单个py文件中下的多个用例用  (类名（"方法名")),
 #导入多个py的类下，用（py名.类名）
 
-
 all_case = '/Users/xintudoutest/appium/AppiumUI/test_case'
-def CreateSuite():                                                        #产生测试套件
-    test_suite = unittest.TestSuite()                                     #使用discover找出用例文件夹下all_test_case的所有用例
-    discover = unittest.defaultTestLoader.discover(all_case,              #查找的文件夹路径
-    pattern = 'ios_*.py', top_level_dir=None)                             #要测试的模块名，以start开头的.py文件
-                                                                          #测试模块的顶层目录，即测试用例不是放在多级目录下，设置为none
+def CreateSuite():                                                      # 产生测试套件
+    test_suite = unittest.TestSuite()                                     # 使用discover找出用例文件夹下all_test_case的所有用例
+    discover = unittest.defaultTestLoader.discover(all_case,              # 查找的文件夹路径
+    pattern = 'ios_*.py', top_level_dir=None)                             # 要测试的模块名，以ios开头的.py文件
 
     for suite in discover:  #使用for循环出suite,再循环出case
         for case in suite:
