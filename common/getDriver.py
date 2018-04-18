4755437a612f0b7b37047ce942d4f5510ffcd073
quanquandequan <quanquandequan@126.com>
2018-04-18 11:44:51 +0800

    Merge branch 'waka'

--- common/getDriver.py
+++ common/getDriver.py
@@ -26,9 +26,10 @@ desired_caps['platformName'] = '%s' % platformName
 desired_caps['platformVersion'] = '%s' % platformVersion
 desired_caps['appPackage'] = '%s' % appPackage
 desired_caps['appActivity'] = '%s' % appActivity
+desired_caps['automationName'] = 'uiautomator2'
 desired_caps['unicodeKeyboard'] = True
 desired_caps['resetKeyboard'] = True
-desired_caps['no-reset'] = True
+# desired_caps['noReset'] = True  #注释之后，每次启动都会自动清除数据
 
 try:
     driver = webdriver.Remote(command_executor="http://127.0.0.1:4723/wd/hub", desired_capabilities=desired_caps)
